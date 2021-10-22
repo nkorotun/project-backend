a252c5c469f77dfa995db6b769d45efeddcee74b
apple <apple@apples-iMac.local>
2021-10-22 12:52:23 +0300

    feature/Login

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthController } from './auth/auth.controller';
+import { UsersController } from './users/users.controller';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [AuthModule, UsersModule],
+  controllers: [AppController, AuthController, UsersController],
   providers: [AppService],
 })
 export class AppModule {}
