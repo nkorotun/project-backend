6cac5b76d0d279b40bf841dfe05cded2c7d3742f
apple <apple@apples-iMac.local>
2021-10-25 15:32:29 +0300

    fix password validaton

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthEntity } from './auth/enteties/auth.entyty';
-import { DB } from './constants';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
     TypeOrmModule.forRoot({
-      type: DB.type,
-      host: DB.host,
-      port: DB.port,
-      username: DB.username,
-      password: DB.password,
-      database: DB.database,
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [AuthEntity],
       synchronize: true,
     }),
